e89639ef47596213e454e4f5853f89eaf90b3220
Felipe  Saruhashi <felipesaruhashi@MBP-de-Felipe.domdmz.einstein.br>
2019-02-22 14:47:13 -0300

    add dots for period mark type

--- src/calendar/day/period/index.js
+++ src/calendar/day/period/index.js
@@ -1,35 +1,34 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import {
-  TouchableWithoutFeedback,
-  Text,
-  View
-} from 'react-native';
-import {shouldUpdate} from '../../../component-updater';
-import isEqual from 'lodash.isequal';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { TouchableWithoutFeedback, Text, View } from "react-native";
+import { shouldUpdate } from "../../../component-updater";
+import isEqual from "lodash.isequal";
 
-import * as defaultStyle from '../../../style';
-import styleConstructor from './style';
+import * as defaultStyle from "../../../style";
+import styleConstructor from "./style";
 
 class Day extends Component {
   static propTypes = {
     // TODO: selected + disabled props should be removed
-    state: PropTypes.oneOf(['selected', 'disabled', 'today', '']),
+    state: PropTypes.oneOf(["selected", "disabled", "today", ""]),
 
     // Specify theme properties to override specific styles for calendar parts. Default = {}
     theme: PropTypes.object,
     marking: PropTypes.any,
 
+    dot: PropTypes.bool,
+    dotColor: PropTypes.string,
+
     onPress: PropTypes.func,
     onLongPress: PropTypes.func,
     date: PropTypes.object,
 
-    markingExists: PropTypes.bool,
+    markingExists: PropTypes.bool
   };
 
   constructor(props) {
     super(props);
-    this.theme = {...defaultStyle, ...(props.theme || {})};
+    this.theme = { ...defaultStyle, ...(props.theme || {}) };
     this.style = styleConstructor(props.theme);
     this.markingStyle = this.getDrawingStyle(props.marking || []);
     this.onDayPress = this.onDayPress.bind(this);
@@ -52,11 +51,16 @@ class Day extends Component {
       return true;
     }
 
-    return shouldUpdate(this.props, nextProps, ['state', 'children', 'onPress', 'onLongPress']);
+    return shouldUpdate(this.props, nextProps, [
+      "state",
+      "children",
+      "onPress",
+      "onLongPress"
+    ]);
   }
 
   getDrawingStyle(marking) {
-    const defaultStyle = {textStyle: {}};
+    const defaultStyle = { textStyle: {} };
     if (!marking) {
       return defaultStyle;
     }
@@ -65,28 +69,28 @@ class Day extends Component {
     } else if (marking.selected) {
       defaultStyle.textStyle.color = this.theme.selectedDayTextColor;
     }
-    const resultStyle = ([marking]).reduce((prev, next) => {
+    const resultStyle = [marking].reduce((prev, next) => {
       if (next.quickAction) {
         if (next.first || next.last) {
           prev.containerStyle = this.style.firstQuickAction;
           prev.textStyle = this.style.firstQuickActionText;
           if (next.endSelected && next.first && !next.last) {
-            prev.rightFillerStyle = '#c1e4fe';
+            prev.rightFillerStyle = "#c1e4fe";
           } else if (next.endSelected && next.last && !next.first) {
-            prev.leftFillerStyle = '#c1e4fe';
+            prev.leftFillerStyle = "#c1e4fe";
           }
         } else if (!next.endSelected) {
           prev.containerStyle = this.style.quickAction;
           prev.textStyle = this.style.quickActionText;
         } else if (next.endSelected) {
-          prev.leftFillerStyle = '#c1e4fe';
-          prev.rightFillerStyle = '#c1e4fe';
+          prev.leftFillerStyle = "#c1e4fe";
+          prev.rightFillerStyle = "#c1e4fe";
         }
         return prev;
       }
 
       const color = next.color;
-      if (next.status === 'NotAvailable') {
+      if (next.status === "NotAvailable") {
         prev.textStyle = this.style.naText;
       }
       if (next.startingDay) {
@@ -119,10 +123,14 @@ class Day extends Component {
     let rightFillerStyle = {};
     let fillerStyle = {};
     let fillers;
+    let dot;
+    let dotStyle = [
+      { width: 4, height: 4, marginTop: 1, borderRadius: 2, opacity: 0 }
+    ];
 
-    if (this.props.state === 'disabled') {
+    if (this.props.state === "disabled") {
       textStyle.push(this.style.disabledText);
-    } else if (this.props.state === 'today') {
+    } else if (this.props.state === "today") {
       containerStyle.push(this.style.today);
       textStyle.push(this.style.todayText);
     }
@@ -131,6 +139,14 @@ class Day extends Component {
       containerStyle.push({
         borderRadius: 17
       });
+
+      if (this.props.marking.dot) {
+        dotStyle.push({
+          opacity: 1,
+          backgroundColor: this.props.marking.dotColor || "#00ffbb"
+        });
+        dot = <View style={dotStyle} />;
+      }
 
       const flags = this.markingStyle;
       if (flags.textStyle) {
@@ -167,10 +183,10 @@ class Day extends Component {
           backgroundColor: flags.endingDay.color
         });
       } else if (flags.day) {
-        leftFillerStyle = {backgroundColor: flags.day.color};
-        rightFillerStyle = {backgroundColor: flags.day.color};
+        leftFillerStyle = { backgroundColor: flags.day.color };
+        rightFillerStyle = { backgroundColor: flags.day.color };
         // #177 bug
-        fillerStyle = {backgroundColor: flags.day.color};
+        fillerStyle = { backgroundColor: flags.day.color };
       } else if (flags.endingDay && flags.startingDay) {
         rightFillerStyle = {
           backgroundColor: this.theme.calendarBackground
@@ -185,8 +201,8 @@ class Day extends Component {
 
       fillers = (
         <View style={[this.style.fillers, fillerStyle]}>
-          <View style={[this.style.leftFiller, leftFillerStyle]}/>
-          <View style={[this.style.rightFiller, rightFillerStyle]}/>
+          <View style={[this.style.leftFiller, leftFillerStyle]} />
+          <View style={[this.style.rightFiller, rightFillerStyle]} />
         </View>
       );
     }
@@ -194,11 +210,15 @@ class Day extends Component {
     return (
       <TouchableWithoutFeedback
         onPress={this.onDayPress}
-        onLongPress={this.onDayLongPress}>
+        onLongPress={this.onDayLongPress}
+      >
         <View style={this.style.wrapper}>
           {fillers}
           <View style={containerStyle}>
-            <Text allowFontScaling={false} style={textStyle}>{String(this.props.children)}</Text>
+            <Text allowFontScaling={false} style={textStyle}>
+              {String(this.props.children)}
+            </Text>
+            {dot}
           </View>
         </View>
       </TouchableWithoutFeedback>
